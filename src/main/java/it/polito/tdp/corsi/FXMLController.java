bcc22100461be9c1efbd59c7411e82b2ebdecac1
carlo <carlo@LAPTOP-M8QN9NCA.station>
2021-04-01 13:33:15 +0200

    Penso sia finito come progetto

--- src/main/java/it/polito/tdp/corsi/FXMLController.java
+++ src/main/java/it/polito/tdp/corsi/FXMLController.java
@@ -5,8 +5,13 @@
 package it.polito.tdp.corsi;
 
 import java.net.URL;
+import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+
+import it.polito.tdp.corsi.model.Corso;
 import it.polito.tdp.corsi.model.Model;
+import it.polito.tdp.corsi.model.Studente;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -46,22 +51,122 @@ public class FXMLController {
 
     @FXML
     void corsiPerPeriodo(ActionEvent event) {
+    	txtRisultato.clear();
+    	String periodoStringa = this.txtPeriodo.getText();
+    	Integer periodo;
+    	
+    	try {
+    		periodo = Integer.parseInt(periodoStringa);
+    	}catch(NumberFormatException ne) {
+    		this.txtRisultato.setText("Devi inserire un numero 1 o 2 per il periodo didattico!");
+    		return;
+    	}catch(NullPointerException npe) {
+    		this.txtRisultato.setText("Devi inserire un numero 1 o 2 per il periodo didattico!");
+    		return;
+    	}if(periodo<1||periodo>2) {
+    		this.txtRisultato.setText("Devi inserire un numero 1 o 2 per il periodo didattico!");
+    		return;
+    	}
+    	//Controllo input fait e finì
+    	
+    	List<Corso> corsi = this.model.getCorsiByPeriodo(periodo);
+    	
+    	/*
+    	for(Corso c : corsi) {
+    		txtRisultato.appendText(c.toString()+"\n");
+    	}
+    	*/
+    	
+    	//PER RENDERE L'OUTPUT MIGLORE -start
+    	txtRisultato.setStyle("-fx-font-family: monospace"); //QUESTA RIGA SI PUO' METTERE NEL SET MODEL!
+    	
+    	StringBuilder sb = new StringBuilder();
+    	for(Corso c: corsi) {
+    		sb.append(String.format("%-8s", c.getCodins()));
+    		sb.append(String.format("%-4s", c.getCrediti()));
+    		sb.append(String.format("%-50s", c.getNome()));
+    		sb.append(String.format("%-4s\n", c.getPd()));
+    	}
+    	
+    	txtRisultato.appendText(sb.toString());
+    	//PER RENDERE L'OUTPUT MIGLORE -end
     	
     }
 
     @FXML
     void numeroStudenti(ActionEvent event) {
+    	txtRisultato.clear();
+    	String periodoStringa = this.txtPeriodo.getText();
+    	Integer periodo;
     	
+    	try {
+    		periodo = Integer.parseInt(periodoStringa);
+    	}catch(NumberFormatException ne) {
+    		this.txtRisultato.setText("Devi inserire un numero 1 o 2 per il periodo didattico!");
+    		return;
+    	}catch(NullPointerException npe) {
+    		this.txtRisultato.setText("Devi inserire un numero 1 o 2 per il periodo didattico!");
+    		return;
+    	}if(periodo<1||periodo>2) {
+    		this.txtRisultato.setText("Devi inserire un numero 1 o 2 per il periodo didattico!");
+    		return;
+    	}
+    	//Controllo input fait e finì
+    	
+    	Map<Corso,Integer> corsiIscrizioni = this.model.getIscrittiByPeriodo(periodo);
+    	for(Corso c : corsiIscrizioni.keySet()) {
+    		txtRisultato.appendText(c.toString());
+    		Integer n = corsiIscrizioni.get(c);
+    		txtRisultato.appendText("\t" + n +"\n");
+    	}
     }
 
     @FXML
     void stampaDivisione(ActionEvent event) {
-
+    	txtRisultato.clear();
+    	
+    	String codice = txtCorso.getText();
+    	
+    	if(!model.esisteCorso(codice)) {
+    		txtRisultato.appendText("Il coros non essiste dio canaglia!");
+    		return;
+    	}
+    	List<Studente> studenti = model.getStudenteByCorso(codice);
+    	
+    	if(studenti.isEmpty()) {
+    		txtRisultato.appendText("Il corso non ha iscritti");
+    		return;
+    	}
+    	
+    	Map<String, Integer> divisione = model.getDivisioneCDS(codice);
+    	
+    	for(String cds : divisione.keySet()) {
+    		txtRisultato.appendText(cds + " " + divisione.get(cds)+"\n");
+    		
+    	}
+    	
     }
 
     @FXML
     void stampaStudenti(ActionEvent event) {
-
+    	txtRisultato.clear();
+    	
+    	String codice = txtCorso.getText();
+    	
+    	if(!model.esisteCorso(codice)) {
+    		txtRisultato.appendText("Il coros non essiste dio canaglia!");
+    		return;
+    	}
+    	List<Studente> studenti = model.getStudenteByCorso(codice);
+    	
+    	if(studenti.isEmpty()) {
+    		txtRisultato.appendText("Il corso non ha iscritti");
+    		return;
+    	}
+    	
+    	for(Studente s: studenti) {
+    		txtRisultato.appendText(s+"\n");
+    	}
     }
 
     @FXML // This method is called by the FXMLLoader when initialization is complete
@@ -79,6 +184,10 @@ public class FXMLController {
     public void setModel(Model model) {
     	this.model = model;
     }
+    
+    //private boolean isValid(String input) {
+    //POtremmo fare un metodo per controllare l'input visto che l'abbuiamo ripetuto 2 volte	
+   // }
     
     
 }
--- src/main/java/it/polito/tdp/corsi/model/Model.java
+++ src/main/java/it/polito/tdp/corsi/model/Model.java
@@ -1,7 +1,60 @@
 package it.polito.tdp.corsi.model;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import it.polito.tdp.corsi.db.CorsoDAO;
 
 public class Model {
 	
+	private CorsoDAO corsoDao;
 	
+	public Model() {
+		corsoDao = new CorsoDAO();
+	}
+	
+	public List<Corso> getCorsiByPeriodo(Integer pd){
+		return corsoDao.getCorsiByPeriodo(pd);
+	}
+	
+	public Map<Corso,Integer> getIscrittiByPeriodo(Integer pd) {
+		return corsoDao.getIscrittiByPeriodo(pd);
+	}
+	
+	public List<Studente> getStudenteByCorso(String codice){
+		return corsoDao.getStudentiByCorso(new Corso(codice, 0, null, 0));
+	}
+
+	public boolean esisteCorso(String codice) {
+		return corsoDao.esisteCorso(new Corso(codice, 0, null, 0));
+	}
+	
+	public Map<String,Integer> getDivisioneCDS(String codice) {
+		//Cosa ci aspettiamo
+		//dato il corso con codice ABC
+		//GEST -> 50
+		//INF -> 40
+		//MEC -> 30
+		
+		
+		/*SOLUZUIONE 1-start
+		Map<String, Integer> divisione =new HashMap<String,Integer>();
+		
+		List<Studente> studenti = this.getStudenteByCorso(codice);
+		for(Studente s: studenti) {
+			if(s.getCDS()!=null && !s.getCDS().equals("")) {
+				if(divisione.get(s.getCDS()) == null) {
+					 divisione.put(s.getCDS(), 1);
+				}else {
+					divisione.put(s.getCDS(), divisione.get(s.getCDS())+1);
+				}
+			}	
+		}
+		return divisione;
+		SOLUZUIONE 1-end*/
+		
+		return corsoDao.getDivisioneStudenti(new Corso(codice,0,null,0));
+		
+	}
 }
